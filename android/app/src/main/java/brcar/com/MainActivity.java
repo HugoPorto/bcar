ed9268d5a6e38e254aeca2a77f689ca30575cdcc
HugoPorto <victor.almeida.porto7@gmail.com>
2024-04-03 20:32:29 -0300

    Ajustando .gitignore

--- android/app/src/main/java/brcar/com/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package brcar.com;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- ref/ref.md
+++
@@ -1,2 +0,0 @@
-npm install --save @capacitor-community/sqlite
-npx cap sync
