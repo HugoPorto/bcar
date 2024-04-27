0346cc949974291b2d754d225a4357a77c0ee0b1
HugoPorto <victor.almeida.porto7@gmail.com>
2024-04-27 11:18:30 -0300

    Update sqlite.services

--- src/app/services/sqlite.service.ts
+++ src/app/services/sqlite.service.ts
@@ -11,6 +11,9 @@ import {
   capSQLiteValues,
 } from '@capacitor-community/sqlite';
 
+/**
+ * Service for interacting with SQLite database.
+ */
 @Injectable()
 export class SQLiteService {
   sqliteConnection!: SQLiteConnection;
@@ -18,10 +21,12 @@ export class SQLiteService {
   platform!: string;
   sqlitePlugin!: CapacitorSQLitePlugin;
   native: boolean = false;
+
   constructor() {}
 
   /**
-   * Plugin Initialization
+   * Initializes the SQLite plugin.
+   * @returns A promise that resolves to true if the plugin is successfully initialized, otherwise false.
    */
   async initializePlugin(): Promise<boolean> {
     this.platform = Capacitor.getPlatform();
@@ -37,6 +42,10 @@ export class SQLiteService {
     return true;
   }
 
+  /**
+   * Initializes the web store for SQLite.
+   * @throws An error if the initialization fails.
+   */
   async initWebStore(): Promise<void> {
     try {
       await this.sqliteConnection.initWebStore();
@@ -46,6 +55,15 @@ export class SQLiteService {
     }
   }
 
+  /**
+   * Opens a database connection.
+   * @param dbName - The name of the database.
+   * @param encrypted - Indicates whether the database is encrypted.
+   * @param mode - The mode to open the database in.
+   * @param version - The version of the database.
+   * @param readonly - Indicates whether the database should be opened in read-only mode.
+   * @returns A promise that resolves to the SQLiteDBConnection object representing the database connection.
+   */
   async openDatabase(
     dbName: string,
     encrypted: boolean,
@@ -76,6 +94,13 @@ export class SQLiteService {
     await db.open();
     return db;
   }
+
+  /**
+   * Retrieves a database connection.
+   * @param dbName - The name of the database.
+   * @param readonly - Indicates whether the database should be retrieved in read-only mode.
+   * @returns A promise that resolves to the SQLiteDBConnection object representing the database connection.
+   */
   async retrieveConnection(
     dbName: string,
     readonly: boolean
@@ -83,17 +108,33 @@ export class SQLiteService {
     return await this.sqliteConnection.retrieveConnection(dbName, readonly);
   }
 
+  /**
+   * Closes a database connection.
+   * @param database - The name of the database to close.
+   * @param readonly - Indicates whether the database is in read-only mode.
+   * @returns A promise that resolves when the connection is closed.
+   */
   async closeConnection(database: string, readonly?: boolean): Promise<void> {
     const readOnly = readonly ? readonly : false;
 
     return await this.sqliteConnection.closeConnection(database, readOnly);
   }
 
+  /**
+   * Adds an upgrade statement to the SQLite plugin.
+   * @param options - The options for the upgrade statement.
+   * @returns A promise that resolves when the upgrade statement is added.
+   */
   async addUpgradeStatement(options: capSQLiteUpgradeOptions): Promise<void> {
     await this.sqlitePlugin.addUpgradeStatement(options);
     return;
   }
 
+  /**
+   * Saves a database to the web store.
+   * @param database - The name of the database to save.
+   * @returns A promise that resolves when the database is saved to the web store.
+   */
   async saveToStore(database: string): Promise<void> {
     return await this.sqliteConnection.saveToStore(database);
   }
